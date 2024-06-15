91918a223021ea90f437d825e84d3f9310ec2876
gGar7 <gautamunofficial50@gmail.com>
2024-06-15 15:56:35 +0530

    added all

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Practice from '../src/components/practice'
+
+
+const App = () => {
+  return (
+    <>
+    < Practice/>
+     
+    
+    </>
+    
+  )
+}
+
+export default App
